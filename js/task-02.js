0fdd0d06db50c7b771f4fc7f6eb1799ab05b724e
Ruslan Garmash <garmash.rb@gmail.com>
2023-02-09 20:21:55 +0200

    update 2 and 6 task

--- js/task-02.js
+++ js/task-02.js
@@ -1,3 +1,17 @@
+// const ingredients = [
+//   "Potatoes",
+//   "Mushrooms",
+//   "Garlic",
+//   "Tomatos",
+//   "Herbs",
+//   "Condiments",
+// ];
+// const ingredientsList = document.querySelector("ul");
+
+// const list = ingredients.reduce((str, item) => str + `<li>${item}</li>`, "");
+
+// ingredientsList.innerHTML = list;
+
 const ingredients = [
   "Potatoes",
   "Mushrooms",
@@ -6,8 +20,13 @@ const ingredients = [
   "Herbs",
   "Condiments",
 ];
-const ingredientsList = document.querySelector("ul");
+const ingredientsEl = document.querySelector("#ingredients");
+const listOfIngredients = ingredients.map((ingredient) => {
+  const listEl = document.createElement("li");
+  listEl.classList.add("item");
+  listEl.textContent = `${ingredient}`;
+  return listEl;
+});
 
-const list = ingredients.reduce((str, item) => str + `<li>${item}</li>`, "");
-
-ingredientsList.innerHTML = list;
+ingredientsEl.append(...listOfIngredients);
+console.log(ingredientsEl);
--- js/task-06.js
+++ js/task-06.js
@@ -1,20 +1,12 @@
 const inputEl = document.querySelector("#validation-input");
 const inputElLength = Number(inputEl.getAttribute("data-length"));
 
-inputEl.addEventListener("blur", ColorInputBlur);
+inputEl.addEventListener("blur", colorInputBlur);
 
-function ColorInputBlur() {
-  if (
-    inputEl.value.length < inputElLength ||
-    inputEl.value.length > inputElLength
-  ) {
-    inputEl.classList.contains("valid")
-      ? inputEl.classList.replace("valid", "invalid")
-      : inputEl.classList.add("invalid");
-  }
-  if (inputEl.value.length === inputElLength) {
-    inputEl.classList.contains("invalid")
-      ? inputEl.classList.replace("invalid", "valid")
-      : inputEl.classList.add("valid");
-  }
+function colorInputBlur() {
+  inputEl.classList.remove("invalid", "valid");
+
+  inputEl.value.length === inputElLength
+    ? inputEl.classList.add("valid")
+    : inputEl.classList.add("invalid");
 }
